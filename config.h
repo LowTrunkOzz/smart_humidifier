6e266cda5c1ff5583f7bc1d4d006c53476488d3b
LowTrunkOzz <56564823+LowTrunkOzz@users.noreply.github.com>
2024-10-23 11:17:39 -0400

    Update config.h
    Final version working

--- config.h
+++ config.h
@@ -1,30 +1,54 @@
 /*
 
 Add all of your definitions and declarations here. 
+For the reed switch -> D7 to COM, NC to GRND!
+
 
 */
-
-
-
 const char* host = "iot-web";
-const char* ssid     = "TellMyWiFiLoveHer_EXT";
-const char* password = "hahasucka!";
+const char* ssid = "YOUR WIFI SSID";
+const char* password = "YOUR WIFI PASSWORD";
+const char* host2 = "script.google.com";
+const int httpsPort = 443;
+Adafruit_MCP23X17 mcp;
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+WiFiClientSecure client;  //--> Create a WiFiClientSecure object.
+
+String GAS_ID = "YOUR SCRIPTS ID FROM GOOGLE SHEETS";  //--> spreadsheet script ID
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-#define DHTPIN1   0    //D3 Digital pin connected to the DHT sensor for top sensor
-#define DHTPIN2  14    //D5 Digital pin connected to the DHT sensor for bottom sensor
-#define humi1 12       //D6 This is top humidifier
-#define humi2 13       //D7 This is bottom humidifier
-#define button 15      //D8 Button for humi controller 
-#define fan1 9         //SD2 Pin for Top section
-#define fan2 10        //SD3  Pin for Bottom section
+#define DHTPIN1 10  //SD3 Digital pin connected to the DHT sensor for top sensor
+#define DHTPIN2 9   //SD2 Digital pin connected to the DHT sensor for bottom sensor
+#define humi1 0     //MCP pin A0, Top humidifier
+#define humi2 1     //MCP pin A1, bottom humidifier
+#define fan1 2      //MCP pin A2
+#define fan2 3      //MCP pin A3
+
+//These lines will turn of serial printing of errors for final code to make it smaller and faster to load
+#if DEBUG == 1
+#define debug(x) Serial.print(x)
+#define debugln(x) Serial.println(x)
+#else
+#define debug(x)
+#define debugln(x)
+#endif
+
+// Set GPIO for reedswitch
+const int reedSwitch = 13;  //D7 on NodeMCU as I couldn't get this to work with the MCP module
+
+
+// Set number of relays
+#define NUM_RELAYS 2
+
+// Assign each GPIO to a relay for the fans to run without the humidifier
+int relayGPIOs[NUM_RELAYS] = { 12, 14 };  //12 is D6, 14 is D5
 
 // Uncomment the type of sensor in use:
-#define DHTTYPE    DHT11     // DHT 11
-//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+//#define DHTTYPE DHT11  // DHT 11
+#define DHTTYPE DHT22  // DHT 22 (AM2302)
+//#define DHTTYPE DHT21 // DHT 21 (AM2301)
