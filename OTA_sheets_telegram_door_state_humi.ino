e61e355dba937c6cd56833f5f8e285ed0be0db02
LowTrunkOzz <56564823+LowTrunkOzz@users.noreply.github.com>
2024-11-04 10:38:58 -0500

    Update OTA_sheets_telegram_door_state_humi.ino

--- OTA_sheets_telegram_door_state_humi.ino
+++ OTA_sheets_telegram_door_state_humi.ino
@@ -56,11 +56,11 @@ bool state;
 String doorState;
 
 // Initialize Telegram BOT
-#define BOTtoken "8174997684:AAGxGfi1qrDue4OVDlsLcFCGoECyspRkjb8"  // your Bot Token (Get from Botfather)
+#define BOTtoken ""  // your Bot Token (Get from Botfather)
 // Use @myidbot to find out the chat ID of an individual or a group
 // Also note that you need to click "start" on a bot before it can
 // message you
-#define CHAT_ID "8062087937"
+#define CHAT_ID ""
 
 X509List cert(TELEGRAM_CERTIFICATE_ROOT);
 WiFiClientSecure client;
